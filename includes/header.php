5c32b90ec13dbba72a62e3cf25185f424231dc10
Steven Yuan <syuan100@gmail.com>
2013-12-07 22:40:30 -0800

    Add includes files, add jquery

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<header>
+
+  <title>Yuanstory</title>
+
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+
+</header>
---
+++ includes/javascript.php
@@ -0,0 +1,6 @@
+<!-- Latest jQuery-->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+
